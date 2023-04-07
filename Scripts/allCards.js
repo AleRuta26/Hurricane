ac0013a377fb64127fce1da494cc793f6ae09e09
AleRuta26 <aleruta2006@gmail.com>
2023-04-07 08:24:13 +0200

    7/04/23
    Bug Fixing and minor graphic changes

--- Scripts/allCards.js
+++ Scripts/allCards.js
@@ -24,24 +24,24 @@ const slug = "xaloc";
 async function main() {
   const graphQLClient = new GraphQLClient("https://api.sorare.com/graphql", {
     headers: {
-      'Authorization': `eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxOWI0OWFkZC01ZWRkLTQzZDEtYThlOS00ZWJhODllZjQyZDYiLCJzY3AiOiJ1c2VyIiwiYXVkIjoiSHVycmljYW5lIiwiaWF0IjoxNjc4NTQ0ODczLCJleHAiOiIxNjgxMTM2ODczIiwianRpIjoiYjViNWZlNWQtYzUwZS00NTdiLWI1ZTItMjY5NjBhNDY0MTgyIn0.H_EkeH-yn0s1mwuMh0jrFTd0toBxpJWzsl_O-b57SBs`,
-      'APIKEY': '1a8f2f62c33a59b503229515eb5c81150fa8ed68ad3be4edf2f65dc2a57f2a92afc0447178bdcf8fde1bcd179672f43df338b4ee8a7e74f100edfbfccf0sr128'
+        'APIKEY': '1a8f2f62c33a59b503229515eb5c81150fa8ed68ad3be4edf2f65dc2a57f2a92afc0447178bdcf8fde1bcd179672f43df338b4ee8a7e74f100edfbfccf0sr128'
     },
   });
 
   let cursor = null;
+  let n = 0;
   do {
-    console.log("Page starting from cursor", cursor);
     const data = await graphQLClient.request(AllCardsFromUser, {
       slug,
       cursor,
     });
     const paginatedCards = data["user"]["paginatedCards"];
     paginatedCards["nodes"].forEach((card) => {
-      console.log(card);
+        n=n+1;
     });
     cursor = paginatedCards["pageInfo"]["endCursor"];
   } while (cursor != null);
+  console.log(n);
 }
 
 main().catch((error) => console.error(error));
--- Scripts/calcioAllCards.js
+++ Scripts/calcioAllCards.js
@@ -1,7 +1,7 @@
 const { GraphQLClient, gql } = require("graphql-request");
 
 const AllCardsFromUser = gql`
-  query AllCardsFromUser($slug: String!, $cursor: String) {
+    query AllCardsFromUser($slug: String!, $cursor: String) {
     user(slug: $slug) {
       paginatedCards(after: $cursor) {
         nodes {
@@ -15,19 +15,17 @@ const AllCardsFromUser = gql`
   }
 `;
 
-const slug = "soraredata";
-const rarity = "limited";
+const slug = "xaloc";
 
 async function main() {
   const graphQLClient = new GraphQLClient("https://api.sorare.com/graphql", {
     headers: {
-      'APIKEY': '1a8f2f62c33a59b503229515eb5c81150fa8ed68ad3be4edf2f65dc2a57f2a92afc0447178bdcf8fde1bcd179672f43df338b4ee8a7e74f100edfbfccf0sr128'
+        'APIKEY': '1a8f2f62c33a59b503229515eb5c81150fa8ed68ad3be4edf2f65dc2a57f2a92afc0447178bdcf8fde1bcd179672f43df338b4ee8a7e74f100edfbfccf0sr128'
     },
   });
 
   let cursor = null;
   let n = 0;
-  let totCards = 0;
   do {
     const data = await graphQLClient.request(AllCardsFromUser, {
       slug,
@@ -35,22 +33,20 @@ async function main() {
     });
     const paginatedCards = data["user"]["paginatedCards"];
     paginatedCards["nodes"].forEach((card) => {
-        card = JSON.stringify(card);
-        card = card.replace('{', '');
-        card = card.replace('}', '');
-        card = card.replace('"rarity":', '');
-        card = card.replace('"', '');
-        card = card.replace('"', '');
-        console.log(card);
-        if (card == "limited"){
+        let card1 = JSON.stringify(card);
+        card1 = card1.replace('{', '');
+        card1 = card1.replace('}', '');
+        card1 = card1.replace('"rarity":', '');
+        card1 = card1.replace('"', '');
+        card1 = card1.replace('"', '');
+        if (card1 == "limited"){
             n=n+1;
         }
     });
-    totCards = totCards + n;
     cursor = paginatedCards["pageInfo"]["endCursor"];
   } while (cursor != null);
-  console.log(totCards)
-  return totCards;
+  console.log(n);
+  return n;
 }
 
 main().catch((error) => console.error(error));
