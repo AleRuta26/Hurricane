0d70a1caf52327979af080e8f01a3ec381c32a98
AleRuta26 <aleruta2006@gmail.com>
2023-04-08 11:27:44 +0200

    8/04/2023
    ETH Count Query created

--- Scripts/allCardsFB.js
+++ Scripts/allCardsFB.js
@@ -1,22 +1,13 @@
 const { GraphQLClient, gql } = require("graphql-request");
 
-const AllCardsFromUser = gql`
-  query AllCardsFromUser($slug: String!, $cursor: String) {
-    user(slug: $slug) {
-      paginatedCards(after: $cursor) {
-        nodes {
-          slug
-          userOwnersWithRate {
-            from
-            price
-          }
-        }
-        pageInfo {
-          endCursor
-        }
-      }
-    }
+const CardNumber = gql`
+query CardNumber($slug: String!) {
+  user(slug: $slug) {
+   cardCounts{
+    limited
   }
+  }
+}
 `;
 
 const slug = "xaloc";
@@ -25,25 +16,26 @@ const slug = "xaloc";
 async function main() {
   const graphQLClient = new GraphQLClient("https://api.sorare.com/graphql", {
     headers: {
-        'APIKEY': '1a8f2f62c33a59b503229515eb5c81150fa8ed68ad3be4edf2f65dc2a57f2a92afc0447178bdcf8fde1bcd179672f43df338b4ee8a7e74f100edfbfccf0sr128'
+      'APIKEY': '1a8f2f62c33a59b503229515eb5c81150fa8ed68ad3be4edf2f65dc2a57f2a92afc0447178bdcf8fde1bcd179672f43df338b4ee8a7e74f100edfbfccf0sr128'
     },
   });
 
-  let cursor = null;
-  let n = 0;
-  do {
-    const data = await graphQLClient.request(AllCardsFromUser, {
-      slug,
-      cursor,
-    });
-    const paginatedCards = data["user"]["paginatedCards"];
-    paginatedCards["nodes"].forEach((card) => {
-        n=n+1;
-    });
-    cursor = paginatedCards["pageInfo"]["endCursor"];
-  } while (cursor != null);
- 
-}
 
+ 
+  const data = await graphQLClient.request(CardNumber, {
+    slug
+  });
+  const cardCounts = data["user"]["cardCounts"];
+  var cardNumber;
+  cardNumber = JSON.stringify(cardCounts);
+  cardNumber = cardNumber.replace('{', '');
+  cardNumber = cardNumber.replace('}', '');
+  cardNumber = cardNumber.replace('limited', '');
+  cardNumber = cardNumber.replace(':', '');
+  cardNumber = cardNumber.replace('"', '');
+  cardNumber = cardNumber.replace('"', '');
+  cardNumber= parseInt(cardNumber);
+  console.log(cardNumber);
+}
 
 main().catch((error) => console.error(error));
